56f22a2f6b8f324a75b1157978f43b57bbdc9a61
Якимов Виктор Игоревич <VIYakimov.SBT@sberbank.ru>
2018-08-09 16:15:25 +0400

    complete 8 lesson

---
+++ Ch02/02_01/start/src/store/reducers.js
@@ -0,0 +1,10 @@
+import C from "../constants"
+
+export const goal = (state = 10, action) => {
+    if(action.type === C.SET_GOAL){
+        return action.payload
+    } else {
+
+        return state
+    }
+}
